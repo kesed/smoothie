67c7411885324a83038e90978a1fc8a87a37d5ac
Kesed Millay <hello@kesedmillay.com>
2016-10-05 17:42:26 -0400

    remove

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@ var image = $("#test");
 
 image.on('load', function(){
   console.info("Image loaded !");
-  $(".quick-load-image").fadeOut();
+  $(".quick-load-image").fadeOut(300, function() { $(this).remove(); });
 });
