f09bf72890c4fbfb1588e57ecd0e6fed85340643
Kesed Millay <hello@kesedmillay.com>
2016-10-06 14:05:50 -0400

    secondary

--- js/secondary.js
+++ js/secondary.js
@@ -1,7 +1,8 @@
 var image = $("img.full-size");
 
-image.on('load', function(){
+$(image).one("load", function() {
   console.log('Image loaded.');
   image.animate({opacity:1}, 1000);
-  //console.log('Opacity animation initialized.');
+}).each(function() {
+  if(this.complete) $(this).load();
 });
