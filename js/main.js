a1f3a850bbb71d906109ccf3f5f71339a136d818
Kesed Millay <hello@kesedmillay.com>
2016-10-06 11:02:10 -0400

    test.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ var image = $("img.full-size");
 
 image.on('load', function(){
   console.log('Image Loaded');
-  image.animate({opacity:1}, 1000);
-  console.log('Opacity Changed');
+  image.animate({opacity:1}, 10000);
+  console.log('Opacity animation initialized.');
 });
