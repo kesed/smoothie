cb6b60423b47dedc2af50e96e80ee4711487dbac
Kesed Millay <hello@kesedmillay.com>
2016-10-06 14:12:10 -0400

    test.

--- js/secondary.js
+++ js/secondary.js
@@ -1,10 +1,14 @@
-var $ =jQuery.noConflict();
-
 var image = $("img.full-size");
 
 $(image).one("load", function() {
   console.log('Image loaded.');
   image.animate({opacity:1}, 1000);
+}).each(function() {
+  if(this.complete) $(this).load();
+});
+
+$("img.full-size").one("load", function() {
+  console.log("second code block triggered");
 }).each(function() {
   if(this.complete) $(this).load();
 });
